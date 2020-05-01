1cb579d630cd168f1c224c7f63a053eef8cbc93d
MauricioSanchezMeza <mau_smm@hotmail.com>
2020-05-01 12:17:38 -0500

    Modificacion V0.0.2 Mauricio S

--- PruebaConGIT/src/mx/com/app/prueba/HolaMundoGit.java
+++ PruebaConGIT/src/mx/com/app/prueba/HolaMundoGit.java
@@ -1,9 +1,15 @@
 package mx.com.app.prueba;
 
+import java.util.Scanner;
+
 public class HolaMundoGit {
 
 	
 	public static void main(String[] args) {
 		System.out.println("Hola mundo con git");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Ingresa tu nombre ");
+		String nombre = sc.next();
+		System.out.println("Bienvenido " + nombre);
 	}
 }
